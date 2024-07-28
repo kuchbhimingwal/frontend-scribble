9919991a26e93678f83e8405c478e24f87bbded4
kuchbhimingwal <shubhammingi@gmail.com>
2024-07-28 12:10:48 -0400

    created the logic for the game wothout canvas!

--- scribble-like-frontend/src/components/Gamepage.tsx
+++ scribble-like-frontend/src/components/Gamepage.tsx
@@ -1,22 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import Messagebox from './messagebox';
 
 function Gamepage() {
   const location = useLocation();
   const socket = location.state;
-  const [playername, setPlayername] = useState("")
+  const [playername, setPlayername] = useState("");
+  const [messages, setMessages] = useState([{}]);
+  const [guessing, setGuessing] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const [word, setWord] = useState("")
   useEffect(()=>{
-
     if (!socket) {
       return;
   }
   socket.onmessage = (event:any) => {
       const message = JSON.parse(event.data);
-
       switch (message.type) {
           case "INIT_GAME":
               setPlayername(message.payload.player)
               break;
+          case "GUESSING_PLAYERS":
+            setGuessing(true);
+            setWord("")
+            break;
+          case "DRAWING":
+            setGuessing(false);
+            setWord(message.payload.word);
+            break;
+          case "TIMER":
+            setTimer(message.payload.time);
+            break;
+          case "GUESS":
+            setMessages((prevArray) => [...prevArray, message.payload]);
+            break;
       }
   }
   },[socket])
@@ -25,9 +42,12 @@ function Gamepage() {
     <div className='gird grid-cols-2'>
       <div className='col-span-1 border border-gray-400'>
         <h1>you are player {playername}</h1>
-
+        {guessing? <h2>you are guessing</h2>: <h2>you are drawing the word:{word}</h2>}
+        <h3>Timer:{timer}</h3>
       </div>
-      <div></div>
+      <div>
+        <Messagebox messages={messages} socket={socket}/>
+      </div>
     </div>
   )
 }
---
+++ scribble-like-frontend/src/components/Messagebox.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+function Messagebox({messages, socket}:any) {
+  const [wordsending, setWordsending] = useState("");
+  const clickHandler = () =>{
+    if(socket){
+      socket.send(JSON.stringify({
+        type: "GUESS",
+        payload: {
+          guess: wordsending
+        }
+    }))
+    }
+  }
+    return (
+    <div>
+      {messages.map((message:any)=>{
+        <div className='flex justify-end'>
+          <h1>{message.player}</h1>
+          <p>{message.guessWord}</p>
+        </div>
+      })}
+      <input type="text" onChange={(e)=>{setWordsending(e.target.value)}}/>
+      <button onClick={clickHandler}>Guess</button>
+    </div>
+  )
+}
+
+export default Messagebox
