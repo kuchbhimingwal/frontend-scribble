4d18bb8062cebdce748e75b55531ba43b3908cae
kuchbhimingwal <shubhammingi@gmail.com>
2024-08-09 13:50:32 -0400

    .

--- scribble-like-frontend/src/App.tsx
+++ scribble-like-frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="home" element={<Home />} />
+            <Route path="" element={<Home />} />
           </Route>
         </Routes>
       </BrowserRouter>
